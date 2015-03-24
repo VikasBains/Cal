d852d27ec5e44d81792c6e13ece6a015943543c9
Vikas Bains <Vikas.Bains@relevancelab.com>
2015-03-24 12:37:15 +0530

    Added clear button

--- cal.rb
+++ cal.rb
@@ -1,15 +1,17 @@
 require 'green_shoes'
 
 Shoes.app(title: "My", width: 200, height: 240) do
-  # number_field = nil
-  # @number = 0
+  number_field = nil
+  @number = 0
   flow width: 200, height: 240 do
     flow width: 0.7, height: 0.2 do
       background rgb(0, 157, 22)
-      #number_field = para @number, margin: 10
+      number_field = para @number, margin: 10
     end
 
     flow width: 0.3, height: 0.2 do
+      button 'Clr', width: 1.0, height: 1.0 do
+      end
     end
 
     flow width: 1.0, height: 0.8 do
